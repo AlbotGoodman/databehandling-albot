f1da552825c04bdb9ca9980dd38f7e875249c4fa
Anders Sanderli <anders.sanderli@iths.se>
2024-11-08 13:28:57 +0100

    todays code-along

--- Code-alongs/241104.dash/dash-api.py
+++ Code-alongs/241104.dash/dash-api.py
@@ -1,33 +1,66 @@
 import os
-from load_data import StockDataAPI
+from load_data import StockDataAPI, StockDataLocal
 import dash
 from dash import html, dcc
 from dash.dependencies import Output, Input
 import plotly_express as px
+from dateutil.relativedelta import relativedelta
 
-api_key = os.getenv('ALPHA_API_KEY')
-stock_data = StockDataAPI(api_key)
-stock_dict = {"AAPL": "Apple", "NVDA": "Nvidia", "TSLA": "Tesla"}
+path = os.path.join(os.path.dirname(__file__), "../../Data/Stocksdata")
+
+# api_key = os.getenv('ALPHA_API_KEY')
+# stock_data = StockDataAPI(api_key)
+stock_data = StockDataLocal(path)
+stock_dict = {"AAPL": "Apple", "NVDA": "Nvidia", "TSLA": "Tesla", "IBM": "IBM"}
+df_dict = {symbol: stock_data.get_dataframe(symbol) for symbol in stock_dict}
 stock_options = [{"label": name, "value": symbol} for symbol, name in stock_dict.items()]    # items hämtar både nycklar och värden
 
 app = dash.Dash(__name__)
+
+ohlc_options = [{"label": option.capitalize(), "value": option} 
+                for option in ["open", "high", "low", "close"]]
+slider_marks = {i: mark for i, mark in enumerate(
+    ["1 day", "1 week", "3 months", "1 year", "5 year", "MAX"])}
 
 app.layout = html.Div([
     html.H1("Stock Viewer"),
     html.P("Choose a stock:"),
     dcc.Dropdown(id="stock-picker-dropdown", className="", options=stock_options,
                  value="AAPL", placeholder="Apple"),
-    dcc.Graph(id="stock_graph")
+    dcc.RadioItems(id="ohlc-radio", className="", 
+                   options=ohlc_options,
+                   value="close"),
+    dcc.Graph(id="stock_graph"),
+    dcc.Slider(id="time-slider", 
+               min=0, 
+               max=5, 
+               step=None, 
+               value=2, 
+               marks=slider_marks)
 ])
 
 @app.callback(
     Output("stock_graph", "figure"),
-    Input("stock-picker-dropdown", "value")
+    Input("stock-picker-dropdown", "value"),
+    Input("ohlc-radio", "value"),
+    Input("time-slider", "value")
 )
-def update_graph(stock):
-    df = stock_data.get_stock_data(stock)
-    fig = px.line(df, df.index, "Close", title=f"Stock price for {stock_dict[stock]}", labels={"index": "Date"}, title=stock)
+def update_graph(stock, ohlc, time_index):
+    df_daily, df_intraday = df_dict[stock]
+    df = df_intraday if time_index < 2 else df_daily
+    days = {i: day for i, day in enumerate([1, 7, 90, 365, 365*5])}
+    df = df if time_index == 5 else filter_time(df, days=days[time_index])
+    fig = px.line(df, x=df.index, y=ohlc, title=stock_dict[stock])
+    # df = stock_data.get_stock_data(stock)
+    # fig = px.line(df, df.index, "Close", title=f"Stock price for {stock_dict[stock]}", labels={"index": "Date"}, title=stock)
     return fig
+
+
+def filter_time(df, days=0):
+    last_day = df.index[0].date()
+    start_day = last_day - relativedelta(days=days)
+    df = df.sort_index().loc[start_day:last_day]
+    return df
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- Code-alongs/241104.dash/load_data.py
+++ Code-alongs/241104.dash/load_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import os
+
 
 class StockDataAPI:
     """Class with methods to get and process data from Alphga Vantage.
@@ -13,11 +15,11 @@ class StockDataAPI:
         self._data_function = data_function
         self._api_key = api_key
 
-    def get_stock_data(self, symbol):
+    def get_stock(self, symbol):
         url = f"https://www.alphavantage.co/query?function={self._data_function}&symbol={symbol}&apikey={self._api_key}&outputsize=full"
         try:
             data = requests.get(url).json()
-            df = pd.DataFrame(data["Time Series (Daily)"]).transpose().astype(float)
+            df = pd.DataFrame(data["Time Series (Daily)"]).transpose().astype(float)        # eftersom vi har en daily tidsserie skulle vi kunna använda TTL cache för att slippa hämta data från API:et varje gång
             df.index = pd.to_datetime(df.index)
             df.columns = ["Open", "High", "Low", "Close", "Volume"]
             return df
@@ -32,9 +34,31 @@ class StockDataAPI:
             df.columns = ["Open", "High", "Low", "Close", "Volume"]
             
             return df
+
+
+class StockDataLocal:
+    """Class with methods to get and process data from local files.
+    """
+
+    def __init__(self, data_folder="Data"):
+        """
+        Args:
+            data_folder: Path to folder with data files
+        """
+        self._data_folder = data_folder
+
+    def get_dataframe(self, stockname):
+        stock_df = []
+        for path_ending in ["_TIME_SERIES_DAILY_ADJUSTED.csv", 
+                     "_TIME_SERIES_INTRADAY_EXTENDED.csv"]:
+            path = os.path.join(self._data_folder, stockname + path_ending)
+            stock = pd.read_csv(path, index_col=0, parse_dates=True)
+            stock.index.rename("Data", inplace=True)
+            stock_df.append(stock)
+        return stock_df
+    
         
 def run_test():
-    import os
     api_key = os.getenv("ALPHA_API_KEY")
     test = StockDataAPI(api_key)
     print(f"Result: {test.get_stock("AAPL")}")
